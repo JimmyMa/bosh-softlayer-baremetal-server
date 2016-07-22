df282a7e36813fc2dfc9e0238cdbdac40903cea2
Michael Maximilien <maxim@us.ibm.com>
2016-07-22 12:04:17 -0700

    added better start.sh and stop.sh to save PID and use it
    Signed-off-by: Michael Maximilien <maxim@us.ibm.com>

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+mkdir -p logs
+
+if [ -f $TMPDIR/bosh-softlayer-bmp.pid ]; then
+	echo "SoftLayer Baremetal server already running, stop it first: bin/stop.sh"
+	exit 0
+fi
+
+echo "Starting SoftLayer Baremetal Server"
+
 nohup bin/api_ctl 1 >> logs/api_ctl.stdout 2 >> logs/api_ctl.stderr &
+
+mktemp -q $TMPDIR/bosh-softlayer-bmp.pid
+echo `ps -A | grep "ruby bin/api_ctl" | head -1 | cut -c 1-5` >> $TMPDIR/bosh-softlayer-bmp.pid
+
 nohup bin/worker_ctl 1 >> logs/worker_1.stdout 2 >> logs/worker_1.stderr &
 nohup bin/worker_ctl 1 >> logs/worker_2.stdout 2 >> logs/worker_2.stderr &
 nohup bin/worker_ctl 1 >> logs/worker_3.stdout 2 >> logs/worker_3.stderr &
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-pkill -9 ruby
+if [ ! -f $TMPDIR/bosh-softlayer-bmp.pid ]; then
+	echo "SoftLayer Baremetal server not running, run it first: bin/start.sh"
+	exit 0
+fi
+
+echo "Stopping SoftLayer Baremetal Server"
+
+BMP_PID=`cat $TMPDIR/bosh-softlayer-bmp.pid`
+
+kill -9 $BMP_PID
+
+rm $TMPDIR/bosh-softlayer-bmp.pid
